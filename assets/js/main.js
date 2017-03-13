3eff18762672831ff9998edfa518e6301e3eb568
Morlauth <tino.caer@me.com>
2017-03-13 19:44:00 -0400

    Mail

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,35 @@
 	});
 
 })(jQuery);
+
+$('#form').on('submit', function(e) {
+	e.preventDefault();
+
+	name = $('.name').val();
+	email = $('.email').val();
+	body = $('.message').val();
+
+	if (name.length < 1 || email.length < 1 || body.length < 1) {
+		// Missing info
+	} else {
+		sendForm(name, email, body)
+	}
+});
+
+function sendForm(name, email, body) {
+	$.ajax({
+		type: 'POST',
+		url: '/email',
+		data: {
+			name: name,
+			email: email,
+			body: body
+		},
+		success: function(res) {
+			// Succesful
+		},
+		error: function(res) {
+			// Error
+		}
+	});
+}
